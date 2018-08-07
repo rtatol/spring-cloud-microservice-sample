2c13974fef038968a3c0fbeff81764aeb7afd452
rtatol <rtatol@gmail.com>
2018-08-07 14:34:08 +0200

    base 'in memory' example

--- api-gateway/src/main/java/com/api/gateway/ApiGatewayApplication.java
+++ api-gateway/src/main/java/com/api/gateway/ApiGatewayApplication.java
@@ -3,6 +3,12 @@ package com.api.gateway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.core.userdetails.MapReactiveUserDetailsService;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.web.server.SecurityWebFilterChain;
 
 @SpringBootApplication
 @EnableDiscoveryClient
@@ -10,5 +16,29 @@ public class ApiGatewayApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ApiGatewayApplication.class, args);
+    }
+
+    @Bean
+    public SecurityWebFilterChain springWebFilterChain(final ServerHttpSecurity http) {
+        return http
+                .httpBasic()
+                .and().csrf().disable()
+                .authorizeExchange()
+                .pathMatchers("/**").authenticated()
+                .anyExchange().permitAll()
+                .and()
+                .build();
+    }
+
+    @Bean
+    public MapReactiveUserDetailsService reactiveUserDetailsService() {
+
+        final UserDetails user = User.withDefaultPasswordEncoder()
+                .username("user")
+                .password("password")
+                .roles("SPRING_CLOUD")
+                .build();
+
+        return new MapReactiveUserDetailsService(user);
     }
 }
